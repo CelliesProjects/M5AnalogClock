ccc682ba30ba96495291fd8337cebd7f2e918793
Cellie <CelliesProjects@users.noreply.github.com>
2019-11-30 20:29:44 +0100

    Give variables more sane names.
    Still only works on M5Stack Fire boards.

--- M5AnalogClock.ino
+++ M5AnalogClock.ino
@@ -1,5 +1,4 @@
 #include <WiFi.h>
-#include <TFT_eSPI.h>
 #include <TFTShape.h>
 #include "Free_Fonts.h"
 
@@ -10,50 +9,51 @@ const char * WIFI_NETWORK = "";
 const char * WIFI_PASSWORD = "";
 
 TFT_eSPI tft = TFT_eSPI();
-TFT_eSprite tft2 = TFT_eSprite(&tft);
+TFT_eSprite  clockSprite = TFT_eSprite(&tft);
 
-void clock1( const uint8_t hour, const uint8_t minute, const uint8_t second, const char * title, const char * footer ) {
-  tft2.fillScreen( TFT_NAVY );
-  tft2.setTextDatum( TC_DATUM ); // Centre text on x,y position
-  tft2.setTextColor( TFT_GREEN );
-  tft2.setFreeFont( FS18 );
-  tft2.drawString( title, 120, 10 );
-  tft2.setFreeFont( FS12 );
-  tft2.drawString( footer, tft.width() / 2, 300 );
+void drawClock( const uint8_t hour, const uint8_t minute, const uint8_t second, const char * title, const char * footer ) {
+  const uint16_t middleH = clockSprite.width() / 2;
+  const uint16_t middleV = clockSprite.height() / 2;
+  clockSprite.fillScreen( TFT_NAVY );
+  clockSprite.setTextColor( TFT_GREEN );
+  clockSprite.setFreeFont( FS18 );
+  clockSprite.drawString( title, middleH, 10 );
+  clockSprite.setFreeFont( FS12 );
+  clockSprite.drawString( footer, middleH, clockSprite.height() - 14 ); //because font size == 12
   TFTShape dot = TFTShapeBuilder::buildNgon(3, 12); dot.setScale(.5, 1);
   TFTShape face = TFTShapeBuilder::buildNgon(12, 100);
-  //clock-face
-  face.draw(&tft2, 120, 160, dot, TFT_WHITE);
+  // clockSprite-face
+  face.draw(&clockSprite, middleH, middleV, dot, TFT_WHITE);
   //hour-hand
   TFTShape hand = TFTShapeBuilder::buildNgon(5, 60);
   uint16_t rotation = map( hour, 0, 12, 0, 360 );//need to add the minutes as an extra angle added between 0 - 30 degrees
   uint16_t extra = map( minute, 0, 60, 0, 30 );
   rotation += extra;
   hand.setScale(.08, .8); hand.setOffset(0, -30); hand.setRotation(rotation);
-  hand.fill(&tft2, 120, 160, TFT_GREY);
+  hand.fill(&clockSprite, middleH, middleV, TFT_GREY);
   //minutes-hand
   rotation = map( minute, 0, 60, 0, 360 );
   hand.setScale(.08, 1); hand.setRotation(rotation);
-  hand.fill(&tft2, 120, 160, TFT_LIGHTGREY);
+  hand.fill(&clockSprite, middleH, middleV, TFT_LIGHTGREY);
   //seconds-hand
   rotation = map( second, 0, 60, 0, 360 );
   hand.setScale(.03, 1); hand.setRotation(rotation);
-  hand.fill(&tft2, 120, 160, TFT_RED);
-  dot.fill(&tft2, 120, 160, TFT_RED);
+  hand.fill(&clockSprite, middleH, middleV, TFT_RED);
+  dot.fill(&clockSprite, middleH, middleV, TFT_RED);
   dot.setScale(.4);
-  dot.fill(&tft2, 120, 160, TFT_DARKGREY);
-  tft2.pushSprite(0, 0);
+  dot.fill(&clockSprite, middleH, middleV, TFT_DARKGREY);
+  clockSprite.pushSprite(0, 0);
 }
 void setup() {
-  if ( WIFI_NETWORK == "" )
-    WiFi.begin();
-  else
-    WiFi.begin( WIFI_NETWORK, WIFI_PASSWORD );
+  WiFi.begin( WIFI_NETWORK, WIFI_PASSWORD );
 
   tft.init();
   tft.setRotation(0);
-  tft.fillScreen(TFT_BLUE);
-  tft2.createSprite(240, 320);
+  tft.fillScreen( TFT_BLUE );
+  tft.setTextDatum( TC_DATUM ); // Centre text on x,y position
+
+  clockSprite.createSprite( 240, 320 );
+  clockSprite.setTextDatum( TC_DATUM ); // Centre text on x,y position
 
   ledcAttachPin( BACKLIGHT, 0);
   ledcSetup( 0, 1300, 16 );
@@ -66,6 +66,8 @@ void setup() {
   struct tm timeinfo = {0};
   while ( !getLocalTime( &timeinfo, 0 ) )
     vTaskDelay( 10 / portTICK_PERIOD_MS );
+
+  tft.drawString( "Ready.", 120, 10 );
 }
 
 void loop() {
@@ -76,7 +78,7 @@ void loop() {
     localtime_r( &now, &localTime );
     char timestr[40];
     strftime( timestr, sizeof( timestr ), "%b %e %Y", &localTime );
-    clock1( localTime.tm_hour, localTime.tm_min, localTime.tm_sec, timestr, "M5Stack sntp clock" );
+    drawClock( localTime.tm_hour, localTime.tm_min, localTime.tm_sec, timestr, "M5Stack sntp  clock" );
     savedTime = now;
   }
   delay( 10 );
